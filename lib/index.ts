b74808079d235d3293a98b03d9aca1ba163dca47
Markel Tuzynskyi <mtuzyn@softserveinc.com>
2021-04-16 09:04:36 +0300

    built

--- lib/index.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-import { runNgOpenApiGen } from './ng-openapi-gen';
-
-// Run the main function
-runNgOpenApiGen()
-  .catch(err => console.error(`Error on API generation: ${err}`));
-
--- test/templates/handlebars.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(handlebars) {
-  // Adding a custom handlebars helper: loud
-  handlebars.registerHelper('loud', function (aString) {
-    return aString.toUpperCase()
-  });
-};
